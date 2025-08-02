c5d4a31fb63f545a578ad0069701f934b3245a9e
jnwync <jonwayne.cabusbusan@gmail.com>
2025-08-03 00:26:25 +0800

    feat: improve login/register UI and add validations

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 import LoginPage from '@/components/Login';
+import SignupForm from '@/components/Login2';
 
 export default function Login() {
-  return <LoginPage />;
+  // return <LoginPage />;
+  return <SignupForm />;
 }
