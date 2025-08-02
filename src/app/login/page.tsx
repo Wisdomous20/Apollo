e296609b717121a3fc1e7c5364baecade77eb84f
jnwync <jonwayne.cabusbusan@gmail.com>
2025-08-03 00:39:49 +0800

    feat: migrate and implement functions and make page dynamic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,5 @@
 import LoginPage from '@/components/Login';
-import SignupForm from '@/components/Login2';
 
 export default function Login() {
-  // return <LoginPage />;
-  return <SignupForm />;
+  return <LoginPage />;
 }
